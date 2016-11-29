f3e7a41fc37cd72e855e7767c622dbf72a838bce
Adam Korczynski <adamkorczynski81@gmail.com>
2016-11-29 21:15:18 +1100

    remove unused files

--- www/offline-iframe/js/offliner.js
+++
@@ -1,9 +0,0 @@
-var offliner = (function() {
-	return {
-		addCachingIframe: function() {
-			$( document.body ).prepend(
-				'<iframe src="/offline-iframe/offline.html" style="position:absolute;top:-999em;visibility:hidden"></iframe>'
-			);
-		}
-	};
-})();
